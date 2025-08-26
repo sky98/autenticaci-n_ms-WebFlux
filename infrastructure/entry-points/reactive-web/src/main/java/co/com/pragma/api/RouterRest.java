d0ea55913d3ea41cf9015be887486250feeaf0f2
sehuanez <>
2025-08-26 09:42:07 -0500

    - Se agrega dependencia para implementar Swagger. - Se realiza refactor del Routerrest para dividir servicios en router personalizados y poder documentar la api sin crecer exponencial la clase donde se expongan

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/RouterRest.java
@@ -1,20 +1,23 @@
 package co.com.pragma.api;
 
+import co.com.pragma.api.router.CrearUsuarioRouter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 @RequiredArgsConstructor
 public class RouterRest {
 
+    private final CrearUsuarioRouter crearUsuarioRouter;
+
     @Bean
-    public RouterFunction<ServerResponse> routerFunction(Handler handler) {
-        return route(POST("/api/v1/usuarios"), handler::guardarUsuario);
+    public RouterFunction<ServerResponse> routerFunction() {
+        return RouterFunctions.route()
+                .add(crearUsuarioRouter.usuarioRouterFunction())
+                .build();
     }
 }
---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/router/CrearUsuarioRouter.java
@@ -0,0 +1,75 @@
+package co.com.pragma.api.router;
+
+import co.com.pragma.api.Handler;
+import co.com.pragma.api.dto.request.CrearUsuarioDTO;
+import co.com.pragma.api.dto.response.UsuarioDTO;
+import co.com.pragma.api.errores.ErrorValidacion;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+
+@Configuration
+@RequiredArgsConstructor
+public class CrearUsuarioRouter {
+
+    public static final String PATH = "/api/v1/usuarios";
+    private final Handler handler;
+
+    @Bean
+    @RouterOperation(
+            path = PATH,
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE
+            },
+            method = org.springframework.web.bind.annotation.RequestMethod.POST,
+            beanClass = Handler.class,
+            beanMethod = "guardarUsuario",
+            operation = @Operation(
+                    operationId = "guardarUsuario",
+                    tags = {"Usuarios"},
+                    summary = "Guarda un nuevo usuario",
+                    requestBody = @RequestBody(
+                            description = "Información del usuario a guardar",
+                            required = true,
+                            content = @Content(
+                                    schema = @Schema(implementation = CrearUsuarioDTO.class)
+                            )
+                    ),
+                    responses = {
+                            @ApiResponse(
+                                    responseCode = "200",
+                                    description = "Usuario guardado exitosamente",
+                                    content = @Content(schema = @Schema(implementation = UsuarioDTO.class))
+                            ),
+                            @ApiResponse(
+                                    responseCode = "400",
+                                    description = "Solicitud inválida",
+                                    content = @Content(
+                                            schema = @Schema(implementation = ErrorValidacion.class),
+                                            examples = @ExampleObject(
+                                                    name = "Ejemplo de respuesta de error 400",
+                                                    value = "{\"status\": 400,\"message\": \"Correo no esta disponible\",\"errors\": [\"correoElectronico\"]}"
+                                            )
+                                    )
+                            )
+                    }
+            )
+    )
+    public RouterFunction<ServerResponse> usuarioRouterFunction() {
+        return RouterFunctions.route(POST(PATH).and(RequestPredicates.accept(MediaType.APPLICATION_JSON)), handler::guardarUsuario);
+    }
+}
